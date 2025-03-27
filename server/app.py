d151ff0abd85fd8bad759135b00b5a79e1e963c3
aaayussuf <aabdikadir849@gmail.com>
2025-03-27 13:51:40 +0300

    completed python-p4-routing-and-views-lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 
+
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)  # Print to console
+    return param
+
+@app.route('/count/<int:num>')
+def count(num):
+    return "\n".join(str(i) for i in range(num)) + "\n"  # Fix: Add trailing newline
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2 if num2 != 0 else "Cannot divide by zero"
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
